79908334909f46fb8cf3ced16b27b60f1e084f62
Danilosalarini <danilosalarini@gmail.com>
2016-02-24 08:52:09 -0300

    Commit inicial

---
+++ maryu/contexto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace maryu
+{
+    class contexto
+    {
+        public static personagem[] enemy = new personagem[3];
+
+        public static personagem hero = new personagem(150, 240);
+
+        public static Texture2D background;
+
+        public static void inicializar(ContentManager content)
+        {
+            hero.textura = content.Load<Texture2D>("vine");
+            background = content.Load<Texture2D>("kermit");
+
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                enemy[i] = new personagem(100 + (i * 200), 140);
+                enemy[i].textura = content.Load<Texture2D>("vine");
+            }
+        }
+        
+    }
+}
---
+++ maryu/personagem.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace maryu
+{
+    class personagem
+    {
+         public int x;
+
+        public int y;
+
+        public Texture2D textura;
+
+        public personagem(int x1, int y1)
+        {
+            x = x1;
+            y = y1;
+        }
+
+        public Vector2 getVector()
+        {
+            return new Vector2(x, y);
+        }
+
+        public void gohorizotal(int pass)
+        {
+            x += pass;
+        }
+
+        public void govertical(int pass)
+        {
+            y += pass;
+        }
+    
+    }
+
+}
